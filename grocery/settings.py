8a1556f64b6c41c2ec539806e39781fde413b80d
tracy <keeyatracy@gmail.com>
2024-10-16 17:55:45 +0300

    added my name

--- grocery/settings.py
+++ grocery/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-m4%+dgsuqjv2!6kth(ttsk$-%+0@rrr0os3kf32i(p8q(p8u55
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['grocery-three-chi.vercel.app']
+ALLOWED_HOSTS = ['grocery-three-chi.vercel.app','grocery-cijugncfh-keeya-tracys-projects.vercel.app']
 
 
 
