3ab5c916f15970c5f13e76ac435d8148de14a0f3
tracy <keeyatracy@gmail.com>
2024-10-16 17:23:39 +0300

    added variable

--- grocery/settings.py
+++ grocery/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 #we are preparing django that our templates are going to use bootstrap4#
 CRISPY_TEMPLATE = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
