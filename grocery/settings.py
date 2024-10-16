9d41b1ff8cfdcb5abb33f7b00683d8d6751a4505
tracy <keeyatracy@gmail.com>
2024-10-16 17:50:36 +0300

    added vercel app

--- grocery/settings.py
+++ grocery/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-m4%+dgsuqjv2!6kth(ttsk$-%+0@rrr0os3kf32i(p8q(p8u55
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['grocery-three-chi.vercel.app']
+
 
 
 # Application definition
