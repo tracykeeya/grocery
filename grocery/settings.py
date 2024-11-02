53a20ec09af9098a53f1ed8c22b6efd201ed46dc
tracy <keeyatracy@gmail.com>
2024-11-02 20:53:04 +0300

    Fix BASE_DIR not defined error in settings.py

--- grocery/settings.py
+++ grocery/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 from constants import ROOT_DIR
+import dj_database_url
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,12 +85,8 @@ WSGI_APPLICATION = 'grocery.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':  os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
-
 
 
 
